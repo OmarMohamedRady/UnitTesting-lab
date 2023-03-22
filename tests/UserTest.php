d36f33d4399097aa9b41b97d04add5b158e4583a
Muhammad Ashraf <muashrafalii@gmail.com>
2023-03-22 15:51:29 +0200

    User Tests

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -4,11 +4,32 @@ require "./src/User.php";
 
 class UserTest extends TestCase{
 
-    public function testZero(){
+    public function testEmptyName(){
 
         $user =new User('Muhammed','Muhammed@gmail.com');
         $result = $user->name();
         $this->assertEquals('Muhammed',$result);
+    }
+
+    public function testParameterizedName(){
+
+        $user =new User('Muhammed','Muhammed@gmail.com');
+        $result = $user->name("Fahmi");
+        $this->assertEquals('Fahmi',$result);
+    }
+
+    public function testEmptyEmail(){
+
+        $user =new User('Muhammed','Muhammed@gmail.com');
+        $result = $user->email();
+        $this->assertEquals('Muhammed@gmail.com',$result);
+    }
+
+    public function testParameterizedEmail(){
+
+        $user =new User('Muhammed','Muhammed@gmail.com');
+        $result = $user->email("Fahmi@gmail.com");
+        $this->assertEquals('Fahmi@gmail.com',$result);
     }
 
 
