02199b2a4f981b1f56f7e7e79eaae8783cd51f35
Ahmed Tahoun <ahmed.tahoun1997@gmail.com>
2023-03-22 15:20:47 +0200

    Third commit

--- tests/FactorialTest.php
+++ tests/FactorialTest.php
@@ -40,9 +40,26 @@ class FactorialTest extends TestCase{
     }
 
 
+    public function testDecimal(){
 
+        $factnum =new Factorial();
+        $result = $factnum->fact(1.5);
+        $this->assertEquals(null,$result);
+    }
+
+    public function testBoolean(){
+
+        $factnum =new Factorial();
+        $result = $factnum->fact(false);
+        $this->assertEquals(null,$result);
+    }
   
+    public function testString(){
 
+        $factnum =new Factorial();
+        $result = $factnum->fact('abc');
+        $this->assertEquals(null,$result);
+    }
    
 
 
